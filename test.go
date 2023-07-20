57dd593aae5c997220930f9d2d497284c3f5c9d0
John Doe <shahiezasauki15@gmail.com>
2023-07-20 10:36:06 +0700

    tes

--- test.go
+++ test.go
@@ -7,10 +7,10 @@ import (
 
 
 func TestInsertmonitor(t *testing.T) {
-	proker := "Pendaftaran"
-	status := "Tidak sesuai target"
-	about := "Membatasi waktu pendaftaran"
-	karyawan := "Kelompok A"
+	proker := "Melakukan maintance website"
+	status := "Dalam pengerjaan"
+	about := "Memperbaiki website karena terdapat kendala"
+	karyawan := "Acep Suhendar"
 	hsl := Insertmonitor(proker, status, about, karyawan)
 	fmt.Println(hsl)
 }
